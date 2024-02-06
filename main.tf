883a533e3b8afbef83ff2f6026d747f9f5328aba
Karunakar-Gyara <52212427+Karunakar-Gyara@users.noreply.github.com>
2024-02-06 15:47:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 resource "google_storage_bucket" "static" {
- name          = "python-practice-407605-new1"
+ name          = "python-practice-407605-new123"
  location      = "US"
  storage_class = "STANDARD"
 
  uniform_bucket_level_access = true
- lifecycle {
-   prevent_destroy = true
- }
+ force_destroy  = false        
 }
 resource "google_compute_instance" "vm" {
   name              = "terraform-instance-1"
