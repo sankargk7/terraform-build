398cafad1fffdb81a1d982a841be1115934862c4
Karunakar-Gyara <52212427+Karunakar-Gyara@users.noreply.github.com>
2024-02-06 15:15:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,21 +8,22 @@ resource "google_storage_bucket" "static" {
    prevent_destroy = true
  }
 }
-resource "google_compute_instance" "vm_instance" {
-name = "terraform-instance2"
-machine_type = "f1-micro"
-zone = "us-central1-c"
-boot_disk {
-initialize_params {
-image = "centos-cloud/centos-7"
-}
-}
+resource "google_compute_instance" "vm" {
+  name              = "terraform-instance-1"
+  machine_type      = "f1-micro"
 
-network_interface {
-network = default
-access_config {
-}
-}
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+  }
 }
 provider "google" {
     project = "python-practice-407605"
