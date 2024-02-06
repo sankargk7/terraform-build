279be6187567d9b152c6984e4a935d5bb99fbb66
Karunakar-Gyara <52212427+Karunakar-Gyara@users.noreply.github.com>
2024-02-06 15:03:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,13 @@ resource "google_storage_bucket" "static" {
    prevent_destroy = true
  }
 }
+resource "google_storage_bucket" "static" {
+ name          = "python-practice-407605-new123"
+ location      = "US"
+ storage_class = "STANDARD"
 
+ uniform_bucket_level_access = true
+}
 provider "google" {
     project = "python-practice-407605"
     credentials = "terraformcloudbuild.json"
