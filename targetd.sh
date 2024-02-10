7901a277bb66164fcbd34dde330594cdc8bbc315
karunakar <karunakargyara74@gmail.com>
2024-02-10 15:41:01 +0530

    file

--- targetd.sh
+++ targetd.sh
@@ -15,6 +15,7 @@ https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update -y
 sudo apt-get install terraform -y
+echo terraform --version
 
 TARGETS=("google_storage_bucket.static")
 
