0be32262be32ec4850b3fdca0b65d3507df20ef0
karunakar <karunakargyara74@gmail.com>
2024-02-10 16:00:33 +0530

    file

---
+++ sqlstartstop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# List of SQL instances to stop
+SQL_INSTANCES=("db-instance-1" "destroy-db "ps-db-1")
+
+
+# Loop through each SQL instance and stop it
+for INSTANCE_NAME in "${SQL_INSTANCES[@]}"
+do
+    # Stop MySQL instance
+    gcloud sql instances patch  "${INSTANCE_NAME}" \
+--activation-policy=ALWAYS 
+
+
+
+    echo "Stopping SQL instance: ${INSTANCE_NAME}"
+done
