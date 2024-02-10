f05dab7d8b5439e043b6b669b7b9409a465d6b50
karunakar <karunakargyara74@gmail.com>
2024-02-10 15:04:28 +0530

    add

--- targetd.sh
+++ targetd.sh
@@ -3,6 +3,19 @@
 
 
 # List of targets (modules and resources)
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update -y
+sudo apt-get install terraform -y
+
 TARGETS=("google_storage_bucket.static")
 
 # Destroy specified targets
@@ -12,6 +25,6 @@ do
 done
 echo ${TARGET_LIST}
 # Run terraform destroy with multiple -target options
-
+terraform destroy "${TARGET_LIST[@]}"
 
 
